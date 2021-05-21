234c0a299198085e378ea467af432c621208edd5
alexandre <xyymenes@egmail.com>
2021-05-20 21:14:47 -0300

    segundo job com @stepScore e #{jobParameters['name']}

--- demo/src/main/java/com/springbatch/demo/Job1/Job2.java
+++ demo/src/main/java/com/springbatch/demo/Job1/Job2.java
@@ -5,15 +5,20 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class Job1 {
+public class Job2 {
+
+    public static final String JOB_PARAMETERS_NAME = "#{jobParameters['name']}";
 
     @Autowired
     private JobBuilderFactory jobBuilderFactory;
@@ -27,21 +32,24 @@ public class Job1 {
         return jobBuilderFactory
                 .get("mJob")
                 .start(imprimeOlaStep())
+                .incrementer(new RunIdIncrementer()) //incrementador automatico do id job
                 .build();
     }
 
     private Step imprimeOlaStep() {
         return stepBuilderFactory
                 .get("imprimeOlaStep")
-                .tasklet(myTasklet())
+                .tasklet(myTasklet(null))
                 .build();
     }
 
-    private Tasklet myTasklet() {
+    @StepScope
+    @Bean
+    public Tasklet myTasklet(@Value(JOB_PARAMETERS_NAME) String name) {
         return new Tasklet() {
             @Override
             public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                System.out.println("imprimindo ola mundo step");
+                System.out.println("Ola mundo, parametro: " +name);
                 return RepeatStatus.FINISHED;
             }
         };

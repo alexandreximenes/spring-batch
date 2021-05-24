dbaea41246ccbcf3bdfbba6a9048081dcf0ca6d0
alexandre <xyymenes@egmail.com>
2021-05-24 03:59:42 -0300

    usando jdbcCursorItemReader e amostra HibernateCursorItemReader

--- demo/src/main/java/com/springbatch/demo/jdbc/reader/JdbcCursorReaderConfig.java
+++ demo/src/main/java/com/springbatch/demo/jdbc/reader/JdbcCursorReaderConfig.java
@@ -1,7 +1,9 @@
 package com.springbatch.demo.jdbc.reader;
 
 import com.springbatch.demo.jdbc.dominio.Cliente;
+import org.springframework.batch.item.database.HibernateCursorItemReader;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.HibernateCursorItemReaderBuilder;
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +14,8 @@ import javax.sql.DataSource;
 
 @Configuration
 public class JdbcCursorReaderConfig {
+
+
 	@Bean
 	public JdbcCursorItemReader<Cliente> jdbcCursorReader(@Qualifier("springDataSource")DataSource dataSource) {
 		return new JdbcCursorItemReaderBuilder<Cliente>()
@@ -21,4 +25,56 @@ public class JdbcCursorReaderConfig {
 				.rowMapper(new BeanPropertyRowMapper<>(Cliente.class))
 				.build();
 	}
+
+	//https://docs.spring.io/spring-batch/docs/current/reference/html/index.html
+//	@Bean
+	public HibernateCursorItemReader<Cliente> jdbcCursorReader2(@Qualifier("springDataSource")DataSource dataSource) {
+//		return new JdbcPagingItemReaderBuilder<Cliente>()
+//		return new HibernateCursorItemReaderBuilder<Cliente>()
+		return new HibernateCursorItemReaderBuilder<Cliente>()
+				.name("jdbcCursorReader")
+				.nativeQuery("SELECT * FROM cliente")
+				.entityClass(Cliente.class)
+				.build();
+	}
+
+	/*
+	* Neo4jItemReader
+	MongoItemReader
+	HibernateCursorItemReader
+	HibernatePagingItemReader
+	RepositoryItemReader
+	SimpleMailMessageItemWriter
+*/
+
+	/*
+	* @Bean
+	public JsonItemReader<Trade> jsonItemReader() {
+	   return new JsonItemReaderBuilder<Trade>()
+                 .jsonObjectReader(new JacksonJsonObjectReader<>(Trade.class))
+                 .resource(new ClassPathResource("trades.json"))
+                 .name("tradeJsonItemReader")
+                 .build();
+	}
+	* */
+
+	/*
+	@Bean
+	public XStreamMarshaller tradeMarshaller() {
+		Map<String, Class> aliases = new HashMap<>();
+		aliases.put("trade", Trade.class);
+		aliases.put("price", BigDecimal.class);
+		aliases.put("isin", String.class);
+		aliases.put("customer", String.class);
+		aliases.put("quantity", Long.class);
+
+		XStreamMarshaller marshaller = new XStreamMarshaller();
+
+		marshaller.setAliases(aliases);
+
+		return marshaller;
+	}*/
+
+	//https://docs.spring.io/spring-batch/docs/current/reference/html/index.html
+
 }
